63dc8c0023ebc734a277b2fb7bf31ceb675bbc79
tan <t.songglod@gmail.com>
2021-08-30 14:46:36 +0700

    [ADD] add week4

--- INT214 week4/04_63130500042.R
+++ INT214 week4/04_63130500042.R
@@ -1,3 +1,21 @@
 library(dplyr)
 
-mtcars$mpg %>% head(9)
+glimpse(starwars)
+
+starwars$height %>% mean(na.rm = T)
+
+starwars %>% select(name, height) %>% head()
+
+#Excluding
+starwars %>% select(-hair_color,-skin_color,-eye_color) %>% head()
+
+starwars %>% filter(sex == "male")
+starwars %>% filter(!(sex == "male")) 
+starwars %>% filter(sex == "male" | skin_color == "light") # or
+starwars %>% filter(sex == "male", skin_color == "light") # and
+
+starwars %>% select(name, height, mass, skin_color) %>% 
+    filter(skin_color == "light")
+
+starwars %>% group_by(sex) %>% select(sex, height) %>%
+    summarise(avg_height = mean(height, na.rm = T))
